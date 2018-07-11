dc1fd8a4ad12e97075989946d0a9feccdc1b0e59
章海文 <zhw@xxlc360.com>
2018-07-11 15:14:57 +0800

    shiro，realm自定义

--- src/test/java/com/zhw/shiroTest.java
+++ src/test/java/com/zhw/shiroTest.java
@@ -1,9 +1,11 @@
 package com.zhw;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 import org.junit.Before;
@@ -23,11 +25,20 @@ public class shiroTest {
     SimpleAccountRealm realm1 = new SimpleAccountRealm();
     SimpleAccountRealm realm2 = new SimpleAccountRealm();
     IniRealm iniRealm = new IniRealm("classpath:userRealm.ini");
+    JdbcRealm jdbcRealm = new JdbcRealm();
+
     @Before
     public void init(){
         realm1.addAccount("zhangsan","123");//认证realm
         realm2.addAccount("zhangsan","123","del","add");//授权realm
 
+        //使用jdbcRealm
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setUrl("");
+        jdbcRealm.setDataSource(dataSource);
+        jdbcRealm.setPermissionsLookupEnabled(true); //启动权限校验
     }
     /**
     *@author: zhanghHaiWen
@@ -93,6 +104,27 @@ public class shiroTest {
         System.out.println(subject.isAuthenticated());
         subject.checkRoles("role1"); //校验角色身份
         subject.checkPermission("user:update");//校验权限
+    }
+
+    /**
+    *@author: zhanghHaiWen
+    *@Desc: 使用jdbcRealm 测试用例
+    *@params:  * @param null
+    *@Date: 2018/7/11 0011 下午 3:12
+    */
+    @Test
+    public void jdbcRealmTest(){
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        securityManager.setRealm(jdbcRealm);
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan","123");
+        subject.login(token);
+        System.out.println(subject.isAuthenticated()); //是否认证
+        subject.checkRole("role1");//校验角色
+        subject.checkPermission("user:delete");//校验用户权限
     }
 
 }
