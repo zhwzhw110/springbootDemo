d63799d17ae2064e47c0ad24f78c2b8c8396c83c
章海文 <zhw@xxlc360.com>
2018-07-11 14:02:32 +0800

    shiro，认证测试授权

--- src/test/java/com/zhw/shiroTest.java
+++ src/test/java/com/zhw/shiroTest.java
@@ -19,16 +19,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class shiroTest {
-    SimpleAccountRealm realm = new SimpleAccountRealm();
+    SimpleAccountRealm realm1 = new SimpleAccountRealm();
+    SimpleAccountRealm realm2 = new SimpleAccountRealm();
+
     @Before
     public void init(){
-        realm.addAccount("zhangsan","123");
+        realm1.addAccount("zhangsan","123");//认证realm
+        realm2.addAccount("zhangsan","123","del");//授权realm
+
     }
+    /**
+    *@author: zhanghHaiWen
+    *@Desc: shiro认证过程
+    *@params:  * @param null
+    *@Date: 2018/7/11 0011 下午 1:55
+    */
     @Test
     public void contextLoads() {
         //构建SecurityManager环境
         DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
-        defaultSecurityManager.setRealm(realm);
+        defaultSecurityManager.setRealm(realm1);
         SecurityUtils.setSecurityManager(defaultSecurityManager);
 
         //获取一个主体
@@ -38,7 +48,27 @@ public class shiroTest {
 
         //判断是否认证
         System.out.println(subject.isAuthenticated());
+    }
 
+    /**
+    *@author: zhanghHaiWen
+    *@Desc: 授权
+    *@params:  * @param null
+    *@Date: 2018/7/11 0011 下午 2:02
+    */
+    @Test
+    public void author(){
+        //构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(realm2);
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+
+        //获取一个主体
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan","123");
+        //授权
+        subject.login(token);
+        subject.checkRole("del");
     }
 
 
